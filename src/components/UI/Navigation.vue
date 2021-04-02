f9a0ffced47e345e41c70d8c18ee8dae008db14a
karljstn <47984349+karljstn@users.noreply.github.com>
2021-04-02 09:29:32 +0200

    Navigation wip

--- src/components/UI/Navigation.vue
+++ src/components/UI/Navigation.vue
@@ -1,38 +1,38 @@
 <template>
-	<nav>
-		<div class="left">
-			<h3>is ai stealing your job.com</h3>
-		</div>
-		<div class="right">
-			<span>menu</span>
-		</div>
-	</nav>
+  <nav>
+    <div class="left">
+      <h3>is ai stealing your job.com</h3>
+    </div>
+    <div class="right">
+      <span>menu</span>
+    </div>
+  </nav>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
 
 export default Vue.extend({});
 </script>
 
 <style lang="scss" scoped>
-@import '../../styles/variables.scss';
+@import "~/styles/_variables.scss";
 
 nav {
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100vw;
-	padding: $menu-padding;
-	display: flex;
-	justify-content: space-between;
-	h3 {
-		width: 150px;
-		transform: rotate(-15deg);
-	}
-	h3,
-	span {
-		font-weight: 200;
-	}
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  padding: $menu-padding;
+  display: flex;
+  justify-content: space-between;
+  h3 {
+    width: 150px;
+    transform: rotate(-15deg);
+  }
+  h3,
+  span {
+    font-weight: 200;
+  }
 }
 </style>
