6a2ddb333786fdcca2f59ed2b84c2d376d69a3a5
dsadsa <ac2031@hotmail.com>
2021-04-27 12:12:27 +0200

    Add links

--- src/views/5_Epilogue/4_EpilogueTakeAction.vue
+++ src/views/5_Epilogue/4_EpilogueTakeAction.vue
@@ -1,10 +1,12 @@
 <template>
 	<section>
-		<a href="#">Link here</a>
-		<a href="#">Link here</a>
-		<a href="#">Link here</a>
-		<a href="#">Link here</a>
-		<a href="#">Link here</a>
+		<a href="https://www.theglobalaisummit.com/">Global AI Summit</a>
+		<a href="https://futureadvocacy.com/ai-think-tank/">AI Think Tank</a>
+		<a href="https://www.impact-ai.fr/">Impact AI</a>
+		<a href="https://futureoflife.org/artificial-intelligence/">Future of life institute</a>
+		<a href="https://www.ibm.com/training/">IBM Training</a>
+		<a href="https://ethics.fast.ai/">Practical Data Ethics - Fast.AI</a>
+
 		<a @click="$router.go(-1)"><Button>x</Button></a>
 	</section>
 </template>
