5039389f5628193b7a2620e7e252461125bb3a59
dsadsa <ac2031@hotmail.com>
2021-05-04 11:39:23 +0200

    Remove slash after tilde

--- src/constants/MODELS.ts
+++ src/constants/MODELS.ts
@@ -1,19 +1,19 @@
-import hand from "~/assets/Models/hand/Hand-anim-v4.glb";
-import trashcan from "~/assets/Models/trashcan/v10_POUBELLE_4.glb";
-import bakedTrashcan from "~/assets/Models/trashcan/bakedTrashcanV2.glb";
-import bakedTrashcanTexture from "~/assets/Models/trashcan/bake2.jpg";
-import clipboard from "~/assets/Games/Radiologist/Models/clipboard_1.glb";
-import emojiSmile from "~/assets/Models/emoji-smile/EMOJI2.glb";
-import emojiSmileBake from "~/assets/Models/emoji-smile/BAKEDFINAL.jpg";
-import emojiGlasses from "~/assets/Models/emoji-glasses/EMOJI.glb";
-import emojiGlassesBake from "~/assets/Models/emoji-glasses/EMOJI1_bake_light.jpg";
-import emojiSad from "~/assets/Models/emoji-sad/EMOJI_2.glb";
-import emojiSadBake from "~/assets/Models/emoji-sad/EMOJI2_bake2.jpg";
-import crystal from "~/assets/Models/crystalball/BOULE_VOYANTE.glb";
-import pencil from "~/assets/Models/pencil/pencil.glb";
-import radiologistBackground from "~/assets/Models/radiologist/big-cube-bg.glb"
-import arrow from "~/assets/Images/fleche.svg"
-import slotMachine from "~/assets/Models/slot-machine/slot_machine_5.glb"
+import hand from "~assets/Models/hand/Hand-anim-v4.glb";
+import trashcan from "~assets/Models/trashcan/v10_POUBELLE_4.glb";
+import bakedTrashcan from "~assets/Models/trashcan/bakedTrashcanV2.glb";
+import bakedTrashcanTexture from "~assets/Models/trashcan/bake2.jpg";
+import clipboard from "~assets/Games/Radiologist/Models/clipboard_1.glb";
+import emojiSmile from "~assets/Models/emoji-smile/EMOJI2.glb";
+import emojiSmileBake from "~assets/Models/emoji-smile/BAKEDFINAL.jpg";
+import emojiGlasses from "~assets/Models/emoji-glasses/EMOJI.glb";
+import emojiGlassesBake from "~assets/Models/emoji-glasses/EMOJI1_bake_light.jpg";
+import emojiSad from "~assets/Models/emoji-sad/EMOJI_2.glb";
+import emojiSadBake from "~assets/Models/emoji-sad/EMOJI2_bake2.jpg";
+import crystal from "~assets/Models/crystalball/BOULE_VOYANTE.glb";
+import pencil from "~assets/Models/pencil/pencil.glb";
+import radiologistBackground from "~assets/Models/radiologist/big-cube-bg.glb"
+import arrow from "~assets/Images/fleche.svg"
+import slotMachine from "~assets/Models/slot-machine/slot_machine_5.glb"
 
 type MODELS = { [name: string]: { URL: string, SCALE: number, TEXTURE?: string } }
 
