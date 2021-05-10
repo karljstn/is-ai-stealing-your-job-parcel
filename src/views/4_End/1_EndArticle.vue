4b75c31c2a6c4418d2708d50af29a382e0b71a73
dsadsa <ac2031@hotmail.com>
2021-05-10 07:20:24 +0200

    Fixes to article

--- src/views/4_End/1_EndArticle.vue
+++ src/views/4_End/1_EndArticle.vue
@@ -1,15 +1,25 @@
 <template>
 	<section>
-		<p>
-			This is a simple example, but it may be closer to the truth than you think.
-		</p>
 		<p>Here's an article on this exact topic.</p>
-		<a href="#"
-			><div class="article"><img src="~/assets/Images/article.jpg" alt="article" /></div
-		></a>
-		<a href="#"
-			><div class="article"><img src="~/assets/Images/text.jpg" alt="article" /></div
-		></a>
+
+		<div class="article">
+			<a href="https://pubmed.ncbi.nlm.nih.gov/29324970/">https://pubmed.ncbi.nlm.nih.gov/29324970/</a>
+			<div class="article__frame">
+				<picture>
+					<source srcset="~/assets/Images/article/container.webp" type="image/webp" />
+					<source srcset="~/assets/Images/article/container.png" type="image/png" />
+					<img src="~/assets/Images/article/container.png" alt="container" />
+				</picture>
+			</div>
+			<div class="article__content">
+				<picture>
+					<source srcset="~/assets/Images/article/pubmed.webp" type="image/webp" />
+					<source srcset="~/assets/Images/article/pubmed.png" type="image/png" />
+					<img src="~/assets/Images/article/pubmed.png" alt="container" />
+				</picture>
+			</div>
+		</div>
+
 		<div class="form">
 			<QuestionForm>
 				<Button value="yes">Next</Button>
@@ -37,14 +47,52 @@ export default Vue.extend({
 
 <style lang="scss" scoped>
 .article {
-	width: 20vw;
+	width: 70vw;
+	max-width: 800px;
+	position: relative;
 
-	img {
-		width: 100%;
+	a {
+		position: absolute;
+		top: 10%;
+		left: 50%;
+		transform: translate(-50%, 0);
+		font-size: 0.8rem;
+		z-index: 1;
 	}
 
-	.form {
-		width: 300px;
+	.article__frame {
+		position: relative;
+		z-index: 1;
+		pointer-events: none;
+		img {
+			width: 100%;
+			box-shadow: 0px 10px 16px 3px rgba(0, 0, 0, 0.2);
+			border-radius: 50px;
+			margin-top: 15px;
+		}
+	}
+
+	.article__content {
+		position: absolute;
+		top: 20%;
+		width: 100%;
+		height: 75.3%;
+		overflow: scroll;
+		z-index: 0;
+		-ms-overflow-style: none; /* IE and Edge */
+		scrollbar-width: none; /* Firefox */
+
+		img {
+			width: 95.7%;
+			position: absolute;
+			overflow: scroll;
+			left: 2.3%;
+		}
+	}
+
+	/* Hide scrollbar for Chrome, Safari and Opera */
+	.article__content::-webkit-scrollbar {
+		display: none;
 	}
 }
 </style>
