eeb84a2f8b3197aae983827bf0c52d2deae82205
karljstn <karljustinianodev@gmail.com>
2021-06-10 16:58:17 +0200

    Mute button + SplitText + Parallax

--- src/components/UI/MuteButton.vue
+++ src/components/UI/MuteButton.vue
@@ -1,83 +1,82 @@
 <template>
-  <div class="bottom">
-    <span ref="mute" v-bind:class="{ active: isMute }" v-on:click="this.mute">{{
-      isMute ? "unmute" : "mute"
-    }}</span>
-  </div>
+    <div class="bottom">
+        <span ref="mute" class="mute-button" v-bind:class="{ activeMute: isMute }" v-on:click="this.mute"
+            >mute</span
+        >
+    </div>
 </template>
 
 <script lang="ts">
-import { Howler } from "howler";
+import { Howler } from "howler"
 export default {
-  data(): { isMute: Boolean } {
-    return {
-      isMute: false,
-    };
-  },
-  mounted() {
-    if (localStorage.getItem("volume") === "0") this.isMute = true;
-    this.setMute();
-  },
-  methods: {
-    mute() {
-      this.isMute = !this.isMute;
-      this.setMute();
+    data(): { isMute: Boolean } {
+        return {
+            isMute: false
+        }
     },
-    setMute() {
-      if (this.isMute) localStorage.setItem("volume", "0");
-      else localStorage.setItem("volume", "1");
+    mounted() {
+        if (localStorage.getItem("volume") === "0") this.isMute = true
+        this.setMute()
+    },
+    methods: {
+        mute() {
+            this.isMute = !this.isMute
+            this.setMute()
+        },
+        setMute() {
+            if (this.isMute) localStorage.setItem("volume", "0")
+            else localStorage.setItem("volume", "1")
 
-      Howler.volume(parseInt(localStorage.getItem("volume")));
-    },
-  },
-};
+            Howler.volume(parseInt(localStorage.getItem("volume")))
+        }
+    }
+}
 </script>
 
 <style lang="scss" scoped>
 @import "~/styles/_variables.scss";
 .white-nav .bottom span {
-  color: $white;
+    color: $white;
 
-  &:after {
-    background-color: $white;
-  }
+    &:after {
+        background-color: $white;
+    }
 }
 .bottom {
-  position: fixed;
-  bottom: 0;
-  padding: $menu-padding;
-  z-index: $menus;
+    position: fixed;
+    bottom: 0;
+    padding: $menu-padding;
+    z-index: $menus;
 
-  span {
-    transition: color 0.5s cubic-bezier(0.55, 0.055, 0.675, 0.19);
-    font-weight: 200;
-    cursor: pointer;
-    position: relative;
+    .mute-button {
+        transition: color 0.5s cubic-bezier(0.55, 0.055, 0.675, 0.19);
+        font-weight: 200;
+        cursor: pointer;
+        position: relative;
 
-    &:hover {
-      &:after {
-        width: 43px;
-      }
+        &:hover {
+            &:after {
+                width: 43px;
+            }
+        }
+
+        &:after {
+            content: "";
+            width: 0px;
+            height: 1px;
+            background-color: $black;
+            position: absolute;
+            top: 13px;
+            left: -2px;
+            transition: all 0.3s;
+        }
     }
 
-    &:after {
-      content: "";
-      width: 0px;
-      height: 1px;
-      background-color: $black;
-      position: absolute;
-      top: 13px;
-      //   top: 20px;
-      left: -2px;
-      transition: all 0.3s;
+    .activeMute {
+        &:after {
+            width: 43px;
+            top: 13px;
+        }
     }
-  }
-
-  .active {
-    &:after {
-      top: 20px;
-      width: 43px;
-    }
-  }
 }
 </style>
--- src/singletons/MouseController.ts
+++ src/singletons/MouseController.ts
@@ -1,52 +1,72 @@
-import { Vector2, Vector3 } from "three";
-import { Viewport } from "~types";
+import { Vector2, Vector3 } from "three"
+import { Viewport } from "~types"
 
 class MouseController {
-  Vec3: Vector3; // Useful for distance and lookAt() logic
-  Vec2: Vector2; // Useful for raycasts
-  Vec3Viewport: Vector3; // Useful for responsive
-  raw: { current: Vector2; previous: Vector2 }; // For the cursor in the game
-  speed: number;
-  hoveredNodeName: string
+    callbacks: Map<string, (e: MouseEvent) => void>
+    Vec3: Vector3 // Useful for distance and lookAt() logic
+    Vec2: Vector2 // Useful for raycasts
+    page: Vector2
+    Vec3Viewport: Vector3 // Useful for responsive
+    raw: { current: Vector2; previous: Vector2 } // For the cursor in the game
+    speed: number
+    hoveredNodeName: string
 
-  constructor() {
-    this.Vec3 = new Vector3();
-    this.Vec2 = new Vector2();
-    this.Vec3Viewport = new Vector3();
-    this.raw = { current: new Vector2(), previous: new Vector2() };
-    this.speed = 0;
-    this.hoveredNodeName = ''
+    constructor() {
+        this.callbacks = new Map<string, (e: MouseEvent) => void>()
 
-    this.setEvents();
-  }
+        this.Vec3 = new Vector3()
+        this.Vec2 = new Vector2()
+        this.Vec3Viewport = new Vector3()
+        this.raw = { current: new Vector2(), previous: new Vector2() }
+        this.speed = 0
+        this.hoveredNodeName = ""
 
-  setFromViewport = (viewport: Viewport) => {
-    this.Vec3Viewport.x = this.Vec3.x * (viewport.width / 2);
-    this.Vec3Viewport.y = this.Vec3.y * (viewport.height / 2);
-  };
+        this.setEvents()
+    }
 
-  mousemove = (e: MouseEvent) => {
-    this.Vec3.x = (e.clientX / window.innerWidth) * 2 - 1;
-    this.Vec3.y = -(e.clientY / window.innerHeight) * 2 + 1;
+    subscribe = (key: string, callback: (e: MouseEvent) => void) => {
+        // console.log("subscribe : ", key);
+        if (this.callbacks.has(key)) console.error(`Duplicate MOUSEMOVE FUNCTION : ${key}`)
 
-    this.Vec2.x = this.Vec3.x;
-    this.Vec2.y = this.Vec3.y;
+        this.callbacks.set(key, callback)
+    }
 
-    this.raw.previous.copy(this.raw.current);
+    unsubscribe = (key: string) => {
+        // console.log("unsubscribe : ", key);
+        if (!this.callbacks.has(key)) console.error(`No such MOUSEMOVE FUNCTION to delete : ${key}`)
 
-    this.raw.current.x = e.clientX;
-    this.raw.current.y = e.clientY;
+        this.callbacks.delete(key)
+    }
 
-    this.speed = this.raw.previous.distanceTo(this.raw.current);
+    setFromViewport = (viewport: Viewport) => {
+        this.Vec3Viewport.x = this.Vec3.x * (viewport.width / 2)
+        this.Vec3Viewport.y = this.Vec3.y * (viewport.height / 2)
+    }
 
-    const target = e.target as HTMLElement
-    this.hoveredNodeName = target.nodeName
-  };
+    mousemove = (e: MouseEvent) => {
+        this.Vec3.x = (e.clientX / window.innerWidth) * 2 - 1
+        this.Vec3.y = -(e.clientY / window.innerHeight) * 2 + 1
 
-  setEvents = () => {
-    window.addEventListener("mousemove", this.mousemove);
-  };
+        this.Vec2.x = this.Vec3.x
+        this.Vec2.y = this.Vec3.y
+
+        this.raw.previous.copy(this.raw.current)
+
+        this.raw.current.x = e.clientX
+        this.raw.current.y = e.clientY
+
+        this.speed = this.raw.previous.distanceTo(this.raw.current)
+
+        const target = e.target as HTMLElement
+        this.hoveredNodeName = target.nodeName
+
+        this.callbacks.forEach(cb => cb(e))
+    }
+
+    setEvents = () => {
+        window.addEventListener("mousemove", this.mousemove)
+    }
 }
 
-const instance = new MouseController();
-export default instance;
+const instance = new MouseController()
+export default instance
